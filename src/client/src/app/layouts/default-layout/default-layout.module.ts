4e9a90298807b29469b9df7ceb6507172ded6a56
hanhhn <hngochanh@outlook.com>
2019-08-04 14:21:59 +0700

    update side bar

--- src/client/src/app/layouts/default-layout/default-layout.module.ts
+++ src/client/src/app/layouts/default-layout/default-layout.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { DefaultModule } from "src/app/pages/default/default.module";
-import { MatSidenavModule } from "@angular/material/sidenav";
 import { DefaultLayoutComponent } from "./default-layout.component";
 import { FooterComponent } from "../../pages/shared/footer/footer.component";
-import { SideBarComponent } from "../../pages/shared/side-bar/side-bar.component";
 import { HeaderModule } from "../../pages/shared/header/header.module";
+import { SideBarModule } from "../../pages/shared/side-bar/side-bar.module";
+import { MatSidenavModule } from "@angular/material/sidenav";
 
 @NgModule({
-  declarations: [DefaultLayoutComponent, FooterComponent, SideBarComponent],
-  imports: [CommonModule, DefaultModule, HeaderModule, MatSidenavModule]
+  declarations: [DefaultLayoutComponent, FooterComponent],
+  imports: [
+    CommonModule,
+    DefaultModule,
+    HeaderModule,
+    SideBarModule,
+    MatSidenavModule
+  ]
 })
 export class DefaultLayoutModule {}

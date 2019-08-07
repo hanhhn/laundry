8629fe787cd8206912b3f69b878cddccffb4b537
Hoàng Ngọc Hạnh <hngochanh@outlook.com>
2019-08-07 13:53:05 +0700

    update

--- src/client/src/app/components/service/service.module.ts
+++ src/client/src/app/components/service/service.module.ts
@@ -4,10 +4,17 @@ import { ServiceComponent } from "./service.component";
 import { MatCardModule } from "@angular/material/card";
 import { MatButtonModule } from "@angular/material/button";
 import { FlexLayoutModule } from "@angular/flex-layout";
+import { MatIconModule } from "@angular/material/icon";
 
 @NgModule({
   declarations: [ServiceComponent],
-  imports: [CommonModule, FlexLayoutModule, MatCardModule, MatButtonModule],
+  imports: [
+    CommonModule,
+    FlexLayoutModule,
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule
+  ],
   exports: [ServiceComponent]
 })
 export class ServiceModule {}

e309848d040caf50654e9dc017d47f7b14977bf4
hanhhn <hngochanh@outlook.com>
2019-08-08 22:41:08 +0700

    update footer

--- src/client/src/app/layouts/default-layout/default-layout.module.ts
+++ src/client/src/app/layouts/default-layout/default-layout.module.ts
@@ -2,17 +2,18 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { DefaultModule } from "src/app/pages/default/default.module";
 import { DefaultLayoutComponent } from "./default-layout.component";
-import { FooterComponent } from "../../pages/shared/footer/footer.component";
 import { HeaderModule } from "../../pages/shared/header/header.module";
 import { SideBarModule } from "../../pages/shared/side-bar/side-bar.module";
 import { MatSidenavModule } from "@angular/material/sidenav";
+import { FooterModule } from "../../pages/shared/footer/footer.module";
 
 @NgModule({
-  declarations: [DefaultLayoutComponent, FooterComponent],
+  declarations: [DefaultLayoutComponent],
   imports: [
     CommonModule,
     DefaultModule,
     HeaderModule,
+    FooterModule,
     SideBarModule,
     MatSidenavModule
   ]
---
+++ src/client/src/app/pages/shared/footer/footer.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { FlexLayoutModule } from "@angular/flex-layout";
+import { FooterComponent } from "./footer.component";
+
+@NgModule({
+  declarations: [FooterComponent],
+  imports: [CommonModule, FlexLayoutModule],
+  exports: [FooterComponent]
+})
+export class FooterModule {}

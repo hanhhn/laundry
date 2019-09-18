5da75f4fb78d4c1345a299d277bf1ecb7df9feb8
hanhhn <hngochanh@outlook.com>
2019-09-18 22:47:16 +0700

    add facebook fanpage

--- src/client/src/app/pages/shared/footer/footer.component.ts
+++ src/client/src/app/pages/shared/footer/footer.component.ts
@@ -1,12 +1,23 @@
-import { Component, Input } from "@angular/core";
+import { Component, Input, OnInit } from "@angular/core";
 import { Company } from "../../../cores/models/setting.model";
+import { FacebookService, InitParams } from "ngx-facebook";
 
 @Component({
   selector: "app-footer",
   templateUrl: "./footer.component.html",
   styleUrls: ["./footer.component.scss"]
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit {
   @Input()
   company: Company;
+
+  constructor(private facebookService: FacebookService) {}
+
+  ngOnInit(): void {
+    this.initFacebookService();
+  }
+  private initFacebookService(): void {
+    const initParams: InitParams = { xfbml: true, version: "v3.2" };
+    this.facebookService.init(initParams);
+  }
 }
--- src/client/src/app/pages/shared/footer/footer.module.ts
+++ src/client/src/app/pages/shared/footer/footer.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { FlexLayoutModule } from "@angular/flex-layout";
 import { FooterComponent } from "./footer.component";
+import { FacebookModule } from "ngx-facebook";
 
 @NgModule({
   declarations: [FooterComponent],
-  imports: [CommonModule, FlexLayoutModule],
+  imports: [CommonModule, FlexLayoutModule, FacebookModule.forRoot()],
   exports: [FooterComponent]
 })
 export class FooterModule {}
